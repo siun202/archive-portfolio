a10728aac63991e5c96afacd840e6950284b70f1
Your Name <avneeshagarwal0612@gmail.com>
2021-09-12 10:21:22 +0530

    fix sidebar links

--- components/header/MenuItem.tsx
+++ components/header/MenuItem.tsx
@@ -22,7 +22,7 @@ const variants = {
 const MenuItem: React.FC = () => {
   return (
     <motion.ul
-      className="list-none flex-col mb-5 flex pl-10 bg-bgblue min-h-screen mt-0 -mr-3 space-y-5 z-50 backdrop-blur-md"
+      className="list-none flex-col mb-5 flex pl-10 bg-bgblue min-h-screen mt-0 -mr-10 space-y-5 z-50 backdrop-blur-md"
       variants={variants}
     >
       <li className="border-b-2 border-transparent cursor-pointer text-text hover:border-neongreen">
@@ -40,20 +40,50 @@ const MenuItem: React.FC = () => {
       <li className="border-b-2 border-transparent cursor-pointer text-text hover:border-neongreen">
         <Link
           activeClass="active"
-          to="work"
+          to="skills"
           spy={true}
           smooth={true}
-          duration={500}
           offset={-100}
+          duration={500}
         >
-          <span className="text-neongreen">2. </span>Work
+          <span className="text-neongreen">2. </span>Skills
         </Link>
       </li>
       <li className="border-b-2 border-transparent cursor-pointer text-text hover:border-neongreen">
-        <span className="text-neongreen">3. </span>Blog
+        <Link
+          activeClass="active"
+          to="work"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
+          <span className="text-neongreen">3. </span>Work
+        </Link>
       </li>
       <li className="border-b-2 border-transparent cursor-pointer text-text hover:border-neongreen">
-        <span className="text-neongreen">4. </span>Contact
+        <Link
+          activeClass="active"
+          to="testimonials"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
+          <span className="text-neongreen">4. </span>Testimonials
+        </Link>
+      </li>{" "}
+      <li className="border-b-2 border-transparent cursor-pointer text-text hover:border-neongreen">
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
+          <span className="text-neongreen">5. </span>Contact
+        </Link>{" "}
       </li>
     </motion.ul>
   );
