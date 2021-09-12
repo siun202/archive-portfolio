505da70fba0a3f965d46b4d6069f65eee3fc5426
Your Name <avneeshagarwal0612@gmail.com>
2021-09-12 08:06:14 +0530

    testimonial transition fix

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -7,16 +7,29 @@ const Testimonials = () => {
       <div className="ml-40">
         <Title num={4} title="Testimonials" />
       </div>
-      <div className="overflow-hidden flex w-screen mx-auto mt-10 space-x-10">
+      <div className="overflow-hidden flex w-screen mx-auto -mb-10 space-x-10">
         {data.testimonials.map((testimonial, i) => (
-          <div
-            key={i}
-            className="bg-darkerblue transform !rotate-45 px-4 pb-3 min-w-[400px] rounded-lg animate-slide"
-            style={{ transform: "rotate(90deg)" }}
-          >
-            <h2 className="mt-5">{testimonial.value}</h2>
-            <h2 className="text-white mt-4">{testimonial.name}</h2>
-            <h2 className="text-white">{testimonial?.relation}</h2>
+          <div key={i} className="animate-slide">
+            <div
+              style={{ transform: `rotate(${i + 1 * 2}deg)` }}
+              className="bg-darkerblue px-4 my-10 pb-3 min-w-[400px] rounded-lg"
+            >
+              <h2 className="pt-5">{testimonial.value}</h2>
+              <h2 className="text-white mt-4">{testimonial.name}</h2>
+              <h2 className="text-white">{testimonial?.relation}</h2>
+            </div>
+          </div>
+        ))}{" "}
+        {data.testimonials.map((testimonial, i) => (
+          <div key={i} className="animate-slide">
+            <div
+              style={{ transform: `rotate(${i + 1 * 2}deg)` }}
+              className="bg-darkerblue px-4 my-10 pb-3 min-w-[400px] rounded-lg"
+            >
+              <h2 className="pt-5">{testimonial.value}</h2>
+              <h2 className="text-white mt-4">{testimonial.name}</h2>
+              <h2 className="text-white">{testimonial?.relation}</h2>
+            </div>
           </div>
         ))}{" "}
       </div>
