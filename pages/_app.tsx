f2bc4f8e114bbda6c9c34396384c8419064e683e
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2021-12-07 21:14:23 +0530

    add keyowrds

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,6 +42,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           site: "@avneesh0612",
           cardType: "summary_large_image",
         }}
+        keywords="Avneesh, Agarwal, Avneesh Agarwal, Blog, web developer,"
       />
       <Component {...pageProps} />
     </>
