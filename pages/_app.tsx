fc853c5df317457c21fff4942b463d121bbe7d84
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2021-12-08 21:54:33 +0530

    add Content Security Policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,6 +51,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           title="RSS"
           href="https://www.avneesh.tech/rss.xml"
         />
+        <meta http-equiv="Content-Security-Policy" content="default-src https://www.avneesh.tech"/>
       </Head>
       <Component {...pageProps} />
     </>
