d833b1833d22f3d4a34713b11468d3fb9f5efda3
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-12-11 13:46:42 +0530

    removed pwa

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching,
-    buildExcludes: [
-      /middleware-manifest\.json$/,
-      /_middleware.js$/,
-      /_middleware.js.map$/,
-    ],
-  },
-
+module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-});
+};
